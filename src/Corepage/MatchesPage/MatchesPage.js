19f989571b3011b960966a6706ec5b01ddfc78b0
Iino Ten <nteten0313@icloud.com>
2019-12-02 20:02:54 +0900

    マッチした動画のコンポーネントを追加

--- src/Corepage/MatchesPage/MatchesPage.js
+++ src/Corepage/MatchesPage/MatchesPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import './MatchesPage.css'
+import Adding_Video from './component/Adding_Video/Adding_Video';
 
 const test_data = [];
 const test_matches_persons = [1,2,3,4,5,6,7,8,9]
@@ -20,6 +21,10 @@ class MatchesPage extends Component{
               </li>
             ))}
           </ul>
+        </div>
+        <div className="match-movies">
+          <h5 className="matched-title">マッチした動画</h5>
+          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
         </div>
       </div>
     );
