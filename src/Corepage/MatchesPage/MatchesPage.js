d1882dc5ecf4793421cb68ab8f8c5f3b1b1c1ed2
Iino Ten <nteten0313@icloud.com>
2019-12-03 03:41:14 +0900

    とりあえず遷移時のアニメーションは保留

--- src/Corepage/MatchesPage/MatchesPage.js
+++ src/Corepage/MatchesPage/MatchesPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 import './MatchesPage.css'
 import Adding_Video from './component/Adding_Video/Adding_Video';
 
@@ -8,30 +8,34 @@ const test_matches_persons = [1,2,3,4,5,6,7,8,9]
 class MatchesPage extends Component{
   render(){
     return(
-      <div className="MatchesPage">
-        <div className="favorites_users">
-          <h5 className="New-tube-title">新しい動画</h5>
-          <ul className="horizontal-list">
-            <li className="item">
-              <div className="New-Clip-icon"></div>
-            </li>
-            {test_matches_persons.map((item) => (
-              <li className="item">
-                <div className="Like-Persons"></div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="match-movies">
-          <h5 className="matched-title">マッチした動画</h5>
-          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
-          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
-          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
-          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
-          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
-          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
-        </div>
-      </div>
+      <TransitionGroup>
+        <CSSTransition key={Math.random} classNames='fade' timeout={2000}>
+           <div className="MatchesPage">
+            <div className="favorites_users">
+              <h5 className="New-tube-title">新しい動画</h5>
+              <ul className="horizontal-list">
+                <li className="item">
+                  <div className="New-Clip-icon"></div>
+                </li>
+                {test_matches_persons.map((item) => (
+                  <li className="item">
+                    <div className="Like-Persons"></div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="match-movies">
+              <h5 className="matched-title">マッチした動画</h5>
+              <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
+              <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
+              <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
+              <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
+              <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
+              <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
+            </div>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
     );
   }
 }
