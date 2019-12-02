12c845f32e21b077770bdb9df9ea542dcdea4121
Iino Ten <nteten0313@icloud.com>
2019-12-02 21:24:08 +0900

    マッチした動画のコンポーネントを追加

--- src/Corepage/MatchesPage/component/Adding_Video/Adding_Video.js
+++ src/Corepage/MatchesPage/component/Adding_Video/Adding_Video.js
@@ -6,6 +6,7 @@ class Adding_Video extends Component{
   constructor(){
     super();
     this.state = {
+      is_HD: true
     }
   }
   render(){
@@ -14,7 +15,15 @@ class Adding_Video extends Component{
     return(
       <div className="Adding_Video">
         <img className="Head-icon" src={this.props.head_img} style={{backgroundImage: `url(${image})`}} />
-        {this.props.test}
+        <div className="Video-description">
+          <p className="Video-Title">Sexy Snapchat Girl</p>
+          {this.state.is_HD? <div className="HD-mark">HD</div>:null}
+          <div>
+            <p className="Video-views">{183}K 再生数</p>
+            <p className="Good-icon"/>
+            <p className="Eval-rate">{80}%</p>
+          </div>
+        </div>
       </div>
     );
   }
