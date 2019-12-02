37f21adfd9b7f38f4185088b2b9bce68293ba5d7
Iino Ten <nteten0313@icloud.com>
2019-12-02 21:24:21 +0900

    クラスを追加

--- src/Corepage/MatchesPage/MatchesPage.js
+++ src/Corepage/MatchesPage/MatchesPage.js
@@ -8,7 +8,7 @@ const test_matches_persons = [1,2,3,4,5,6,7,8,9]
 class MatchesPage extends Component{
   render(){
     return(
-      <div>
+      <div className="MatchesPage">
         <div className="favorites_users">
           <h5 className="New-tube-title">新しい動画</h5>
           <ul className="horizontal-list">
@@ -24,6 +24,11 @@ class MatchesPage extends Component{
         </div>
         <div className="match-movies">
           <h5 className="matched-title">マッチした動画</h5>
+          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
+          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
+          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
+          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
+          <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
           <Adding_Video head_img={'https://ci.phncdn.com/videos/201910/31/258477242/original/(m=e0YHGgaaaa)(mh=SB6Y_qD6E4asab8I)3.jpg'} test="test"/>
         </div>
       </div>
