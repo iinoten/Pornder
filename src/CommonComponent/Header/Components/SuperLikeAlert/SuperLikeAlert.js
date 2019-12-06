262c4337e913d5e9a3208f626db7214d3cf7b31a
Iino Ten <nteten0313@icloud.com>
2019-12-06 11:42:10 +0900

    テキストを修正

--- src/CommonComponent/Header/Components/SuperLikeAlert/SuperLikeAlert.js
+++ src/CommonComponent/Header/Components/SuperLikeAlert/SuperLikeAlert.js
@@ -19,6 +19,7 @@ class SuperLikeAlert extends Component{
     return(
       <div className="SuperLikeAlert">
         <h4 className="yet_alert-text">この機能はこれから実装予定</h4>
+        <h6 className="yet_alert-subtext">実はどんな機能をくっつけるかも考えれてないかも</h6>
         <div className="SuperLikeAlert-Icon"><div className="gray_back" /></div>
       </div>
     );
