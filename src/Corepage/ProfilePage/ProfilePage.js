3131d0aa8c76852b1ca6758842ae0a4227e27c54
Iino Ten <nteten0313@icloud.com>
2019-12-06 11:39:01 +0900

    設定ボタンもポップアップやるようにした

--- src/Corepage/ProfilePage/ProfilePage.js
+++ src/Corepage/ProfilePage/ProfilePage.js
@@ -12,13 +12,16 @@ class ProfilePage extends Component{
   onPush_SuperLike_Button_handler = () => {
     this.props.popup_yet_alert();
   }
+  onPush_YET_setting_button = () => {
+    this.props.popup_yet_alert();
+  }
   render(){
     return(
       <div className="ProfilePage">
         <div className="Main-icon" />
         <div className="Setting_buttons">
-          <button className="EditButton Config" />
-          <button className="EditButton Info" />
+          <button onClick={this.onPush_YET_setting_button} className="EditButton Config" />
+          <button onClick={this.onPush_YET_setting_button} className="EditButton Info" />
         </div>
         <div><button className="Add-clip-Button"><div className="Mini_mark"/></button></div>
         <div className="Functional_Buttons">
