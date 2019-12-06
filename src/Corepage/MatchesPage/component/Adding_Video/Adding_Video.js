11772046040501e762801290bbafe9511f849652
Iino Ten <nteten0313@icloud.com>
2019-12-07 07:35:13 +0900

    表示するときのタイトルの文字数を制限

--- src/Corepage/MatchesPage/component/Adding_Video/Adding_Video.js
+++ src/Corepage/MatchesPage/component/Adding_Video/Adding_Video.js
@@ -10,11 +10,12 @@ class Adding_Video extends Component{
     }
   }
   render(){
+    console.log()
     return(
       <a target='_blank' rel="noopener noreferrer" href={this.props.url} className="Adding_Video">
         <img alt="えっちなアイコン" className="Head-icon" src={this.props.head_img} style={{backgroundImage: `url(${image})`}} />
         <div className="Video-description">
-          <p className="Video-Title">{this.props.title}</p>
+          <p className="Video-Title">{this.props.title.slice(0,25) + (this.props.title.slice(25) ? '...':'')}</p>
           {this.state.is_HD? <div className="HD-mark">HD</div>:null}
           <div>
             <p className="Video-views">{Math.floor(this.props.views/ 1000)}K 再生数</p>
