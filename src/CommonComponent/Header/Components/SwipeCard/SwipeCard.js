131f17b6d1e608e2c17ec0ad784b359f9040cd54
Iino Ten <nteten0313@icloud.com>
2019-12-07 06:07:56 +0900

    動画読み込み時のカードを修正

--- src/CommonComponent/Header/Components/SwipeCard/SwipeCard.js
+++ src/CommonComponent/Header/Components/SwipeCard/SwipeCard.js
@@ -6,12 +6,14 @@ class SwipeCard extends Component{
   render(){
     return(
       <div className="Card">
-        <img alt="エッチなサムネ" className="Card_image" src={this.props.img_src} />
+        {this.props.img_src?<img alt="エッチなサムネ" className="Card_image" src={this.props.img_src} />:null}
         <div className="Card-description">
           <h5 className="Video-title">{this.props.Video_title}</h5>
+          { this.props.rating ?
           <div className="Card-Sub-descripntion">
             <p className="Card-Good_icon"/><p className="Card-Eval-rate">{this.props.rating}%</p>
           </div>
+          : null}
         </div>
       </div>
     );
