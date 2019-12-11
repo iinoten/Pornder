91a4ac2a5e98d266521f444534f31a6ca3ae5594
Iino Ten <nteten0313@icloud.com>
2019-12-12 06:16:13 +0900

    モック用データの表示

--- src/Corepage/EditPage/EditPage.js
+++ src/Corepage/EditPage/EditPage.js
@@ -2,6 +2,17 @@ import React, {Component} from 'react';
 import {withRouter} from 'react-router';
 import './EditPage.css'
 
+const recomended_categories = [
+  'japanese',
+  'mature',
+  'anal',
+  'babe',
+  'amateur',
+  'threesome',
+  'MLF',
+  'lesbian'
+]
+
 class EditPage extends Component{
   constructor(){
     super();
@@ -16,6 +27,28 @@ class EditPage extends Component{
         <div className='EditPage-Header'>
           <div className='EditPage-Header_Title'>情報の編集</div>
           <button className='EditPage-Header_backbutton' onClick={this.onPush_Done_button_handler}>完了</button>
+        </div>
+        <div className='EditPage-recomended-categories'>
+          <h4 className='EditPage_Description'>これが今、あなたにオススメしているカテゴリです</h4>
+          <table>
+            <tbody>
+              <tr>
+                {recomended_categories.map((item, i) => {
+                  if(i<=2) return <td>{item}</td>
+                })}
+              </tr>
+              <tr>
+                {recomended_categories.map((item, i) => {
+                  if(i>=3&&i<=5) return <td>{item}</td>
+                })}
+              </tr>
+              <tr>
+                {recomended_categories.map((item, i) => {
+                  if(i>=6&&i<=8) return <td>{item}</td>
+                })}
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     );
